5d16983af44a07415768248dc192c7b4a17bff20
tramiNGY <ngytrami@gmail.com>
2025-07-01 01:36:30 +0200

    Added grabPoint for each grabbable object hold area

--- Assets/Scripts/Objects/GrabbableObject.cs
+++ Assets/Scripts/Objects/GrabbableObject.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class GrabbableObject : MonoBehaviour
 {
+    public Transform grabPoint;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
