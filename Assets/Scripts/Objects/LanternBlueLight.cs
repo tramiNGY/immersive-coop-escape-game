27fc6abf919f08742e0f40b7acb06693b87b8197
tramiNGY <ngytrami@gmail.com>
2025-07-02 21:14:16 +0200

    Made Lantern Light color, intensity and range transition smoothly with Lerp

--- Assets/Scripts/Objects/LanternBlueLight.cs
+++ Assets/Scripts/Objects/LanternBlueLight.cs
@@ -8,6 +8,9 @@ public class LanternBlueLight : MonoBehaviour
     [SerializeField] private Light LanternLight4;
     [SerializeField] private Collider CompassCollider;
     [SerializeField] private Color MagicLanternLight = Color.blue;
+    [SerializeField] private float LanternTransitionSpeed = 1f;
+    private bool LanternChangeColor = false;
+    
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -18,26 +21,28 @@ public class LanternBlueLight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (LanternChangeColor)
+        {
+            LanternLight1.color = Color.Lerp(LanternLight1.color, MagicLanternLight, Time.deltaTime * LanternTransitionSpeed);
+            LanternLight1.intensity = Mathf.Lerp(LanternLight1.intensity, 0.8f, Time.deltaTime * LanternTransitionSpeed);
+            LanternLight1.range = Mathf.Lerp(LanternLight1.range, 2f, Time.deltaTime * LanternTransitionSpeed);
+            LanternLight2.color = Color.Lerp(LanternLight2.color, MagicLanternLight, Time.deltaTime * LanternTransitionSpeed);
+            LanternLight2.intensity = Mathf.Lerp(LanternLight2.intensity, 0.8f, Time.deltaTime * LanternTransitionSpeed);
+            LanternLight2.range = Mathf.Lerp(LanternLight2.range, 2f, Time.deltaTime * LanternTransitionSpeed);
+            LanternLight3.color = Color.Lerp(LanternLight3.color, MagicLanternLight, Time.deltaTime * LanternTransitionSpeed);
+            LanternLight3.intensity = Mathf.Lerp(LanternLight3.intensity, 0.8f, Time.deltaTime * LanternTransitionSpeed);
+            LanternLight3.range = Mathf.Lerp(LanternLight3.range, 2f, Time.deltaTime * LanternTransitionSpeed);
+            LanternLight4.color = Color.Lerp(LanternLight4.color, MagicLanternLight, Time.deltaTime * LanternTransitionSpeed);
+            LanternLight4.intensity = Mathf.Lerp(LanternLight4.intensity, 0.8f, Time.deltaTime * LanternTransitionSpeed);
+            LanternLight4.range = Mathf.Lerp(LanternLight4.range, 2f, Time.deltaTime * LanternTransitionSpeed);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other == CompassCollider)
         {
-            LanternLight1.color = MagicLanternLight;
-            LanternLight1.intensity = 0.8f;
-            LanternLight1.range = 1.5f;
-            LanternLight2.color = MagicLanternLight;
-            LanternLight2.intensity = 0.8f;
-            LanternLight2.range = 1.5f;
-            LanternLight3.color = MagicLanternLight;
-            LanternLight3.intensity = 0.8f;
-            LanternLight3.range = 1.5f;
-            LanternLight4.color = MagicLanternLight;
-            LanternLight4.intensity = 0.8f;
-            LanternLight4.range = 1.5f;
-            
+            LanternChangeColor = true;
         }
     }
 }
