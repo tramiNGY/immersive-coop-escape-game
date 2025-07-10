32f57abedc0e94bdea5a9cbfa8e7ea93fbf3cf3b
tramiNGY <ngytrami@gmail.com>
2025-07-11 01:06:19 +0200

    removed currentscene serializefield to getactivescene for each current scene and prevent reloading same scene

--- Assets/Scripts/Managers/SceneLoader.cs
+++ Assets/Scripts/Managers/SceneLoader.cs
@@ -5,15 +5,22 @@ using UnityEngine.UI;
 public class SceneLoader : MonoBehaviour
 {
     public Image SceneTransitionImage;
-    [SerializeField] private string currentScene = "";
+    private string currentScene;
 
     private void Awake()
     {
         DontDestroyOnLoad(gameObject); // Make SceneManager object persistent in all scenes
+        currentScene = SceneManager.GetActiveScene().name;
     }
-    
+
     public void LoadScene(string newScene)
     {
+        if (SceneManager.GetSceneByName(newScene).isLoaded) // prevents reloading same scene
+        {
+            Debug.Log($"Scene {newScene} is already loaded");
+            return;
+        }
+
         SceneTransitionImage.gameObject.SetActive(true);
         SceneTransitionImage.color = new Color(0, 0, 0, 1f); // RGB + alpha: 0 transparent -> 1 opaque
 
