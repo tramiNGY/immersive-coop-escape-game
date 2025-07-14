25801378620b5eda6e6aafa0f6e00e8c5b25992a
tramiNGY <ngytrami@gmail.com>
2025-07-14 10:15:27 +0200

    Fixed runes fade from left to right, from 0 (invisible) to 1f (visible)

--- Assets/Scripts/Puzzle/Room1/RunesAppear.cs
+++ Assets/Scripts/Puzzle/Room1/RunesAppear.cs
@@ -13,7 +13,7 @@ public class RunesAppear : MonoBehaviour
         var renderer = GetComponent<Renderer>();
         runesMaterialInstance = renderer.material; // local instance of mat to avoid alter mat prefab
 
-        runesMaterialInstance.SetFloat("_FadeAmount", 1f); // start with invisible runes
+        runesMaterialInstance.SetFloat("_FadeAmount", 0f); // start with invisible runes
     }
 
     void Update()
@@ -22,9 +22,9 @@ public class RunesAppear : MonoBehaviour
         {
             timer += Time.deltaTime; // adds time since last frame, so fading progress overtime
             float fade = Mathf.Clamp01(timer / wipeDuration); // calculates how much time/total fade time, Clamp01 keeps value between 0 and 1
-            runesMaterialInstance.SetFloat("_FadeAmount", 1f - fade); // fade from 1(invisible) to 0 (visible), fade increase each frame closer to 1
+            runesMaterialInstance.SetFloat("_FadeAmount", fade); // fade from 0(invisible) to 1 (visible), fade increase each frame closer to 1
 
-            if (fade >= 1f) // when fade reaches one and FadeAmount is at 0 (visible)
+            if (fade >= 1f) // when FadeAmount reaches 1 (fully visible)
                 wiping = false;  // condition stop update fade
         }
     }
